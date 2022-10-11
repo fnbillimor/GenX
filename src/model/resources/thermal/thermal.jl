c9612432800d03417221303c09bfd6660d7a0653
sambuddhac <sambuddha.chakrabarti@gmail.com>
2022-10-10 23:38:51 -0400

    Added stochastic scenarios to VRE curtailable and thermal

--- src/model/resources/thermal/thermal.jl
+++ src/model/resources/thermal/thermal.jl
@@ -8,6 +8,7 @@ function thermal!(EP::Model, inputs::Dict, setup::Dict)
 
 	T = inputs["T"]     # Number of time steps (hours)
 	Z = inputs["Z"]     # Number of zones
+	SC = inputs["SC"]   # Number of scenarios
 
 	THERM_COMMIT = inputs["THERM_COMMIT"]
 	THERM_NO_COMMIT = inputs["THERM_NO_COMMIT"]
@@ -23,8 +24,8 @@ function thermal!(EP::Model, inputs::Dict, setup::Dict)
 		thermal_no_commit!(EP, inputs, setup)
 	end
 	##CO2 Polcy Module Thermal Generation by zone
-	@expression(EP, eGenerationByThermAll[z=1:Z, t=1:T], # the unit is GW
-		sum(EP[:vP][y,t] for y in intersect(inputs["THERM_ALL"], dfGen[dfGen[!,:Zone].==z,:R_ID]))
+	@expression(EP, eGenerationByThermAll[z=1:Z, t=1:T, sc=1:SC], # the unit is GW
+		sum(EP[:vP][y,t,sc] for y in intersect(inputs["THERM_ALL"], dfGen[dfGen[!,:Zone].==z,:R_ID]))
 	)
 	EP[:eGenerationByZone] += eGenerationByThermAll
 
