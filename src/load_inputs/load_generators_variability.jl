305f969a5a45303435d0b2744c7f1588686727f8
Chakrabarti, Sambuddha (Sam) <sambuddha.chakrabarti@gmail.com>
2023-05-16 18:56:35 -0400

    Modified load, fuel, and generators variability data loading scripts

--- src/load_inputs/load_generators_variability.jl
+++ src/load_inputs/load_generators_variability.jl
@@ -7,29 +7,35 @@ function load_generators_variability!(setup::Dict, path::AbstractString, inputs:
 
 	# Hourly capacity factors
 	data_directory = joinpath(path, setup["TimeDomainReductionFolder"])
-    if setup["TimeDomainReduction"] == 1  && time_domain_reduced_files_exist(data_directory)
-        my_dir = data_directory
+    	if setup["TimeDomainReduction"] == 1  && time_domain_reduced_files_exist(data_directory)
+        	my_dir = data_directory
 	else
-        my_dir = path
+        	my_dir = path
 	end
-    filename = "Generators_variability.csv"
-    gen_var = load_dataframe(joinpath(my_dir, filename))
 
-    all_resources = inputs["RESOURCES"]
+    	filename = "Generators_variability_scenario_*.csv"
+    	gen_var_files = glob(filename, my_dir)
+	gen_var = DataFrame.(CSV.File.(gen_var_files))
 
-    existing_variability = names(gen_var)
-    for r in all_resources
-        if r ∉ existing_variability
-            @info "assuming availability of 1.0 for resource $r."
-            ensure_column!(gen_var, r, 1.0)
-        end
-    end
+	all_resources = inputs["RESOURCES"]
 
-	# Reorder DataFrame to R_ID order (order provided in Generators_data.csv)
-	select!(gen_var, [:Time_Index; Symbol.(all_resources) ])
+	
+	for i in 1:length(gen_var)
+		existing_variability = names(gen_var[i])
+		for r in all_resources
+			if r ∉ existing_variability
+				@info "assuming availability of 1.0 for resource $r."
+				ensure_column!(gen_var[i], r, 1.0)
+	    		end
+		end
+		# Reorder DataFrame to R_ID order (order provided in Generators_data.csv)
+		select!(gen_var[i], [:Time_Index; Symbol.(all_resources) ])
+    
+		# Maximum power output and variability of each energy resource
+		inputs["pP_Max_scenario_$i"] = transpose(Matrix{Float64}(gen_var[i][1:inputs["T"],2:(inputs["G"]+1)]))
+	end
 
-	# Maximum power output and variability of each energy resource
-	inputs["pP_Max"] = transpose(Matrix{Float64}(gen_var[1:inputs["T"],2:(inputs["G"]+1)]))
-
+    
 	println(filename * " Successfully Read!")
+
 end
