ea1c09cb1f9e7f0eb01e4c2afc251d002624e87f
Chakrabarti, Sambuddha (Sam) <sambuddha.chakrabarti@gmail.com>
2023-05-30 14:41:54 -0400

    Added modified probability distribution loading, generators variability, loads, and network

--- src/load_inputs/load_load_data.jl
+++ src/load_inputs/load_load_data.jl
@@ -1,25 +1,9 @@
-"""
-GenX: An Configurable Capacity Expansion Model
-Copyright (C) 2021,  Massachusetts Institute of Technology
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-A complete copy of the GNU General Public License v2 (GPLv2) is available
-in LICENSE.txt.  Users uncompressing this from an archive may not have
-received this license file.  If not, see <http://www.gnu.org/licenses/>.
-"""
-
 @doc raw"""
-	load_data(setup::Dict, path::AbstractString, inputs_load::Dict)
+	load_data!(setup::Dict, path::AbstractString, inputs::Dict)
 
-Function for reading input parameters related to electricity load (demand)
+Read input parameters related to electricity load (demand)
 """
-function load_load_data(setup::Dict, path::AbstractString, inputs_load::Dict)
+function load_load_data!(setup::Dict, path::AbstractString, inputs::Dict)
 	# Load related inputs
 	data_directory = joinpath(path, setup["TimeDomainReductionFolder"])
     	if setup["TimeDomainReduction"] == 1  && time_domain_reduced_files_exist(data_directory)
